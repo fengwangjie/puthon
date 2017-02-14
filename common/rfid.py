68d70f77e7428d50d816a11ef139ddb02f830702
fengwangjie@wisdomgarden.com <fengwangjie@wisdomgarden.com>
2017-02-14 15:17:16 +0800

    move common test class to tests dir

--- common/rfid.py
+++ common/rfid.py
@@ -1,5 +1,6 @@
 import codecs
 import struct
 
+
 def encode(raw_card):
     return codecs.encode(struct.pack("<I", int(raw_card)), 'hex').upper()
--- common/tests/diff_test.py
+++ common/tests/diff_test.py
@@ -2,16 +2,15 @@ from common.diff import DictDiffer
 
 
 def test_dict_differ_with_change():
-    old = { 'academic_term': "2016", "academic_term": "2", "id": 1}
-    new = { 'academic_term': "2016", "academic_term": "1"}
+    old = {'academic_term': "2016", "academic_term": "2", "id": 1}
+    new = {'academic_term': "2016", "academic_term": "1"}
     dd = DictDiffer(old, new)
     assert len(dd.changed()) == 1
     assert dd.changed().pop() == 'academic_term'
 
 
 def test_dict_differ_without_change():
-    old = { 'academic_term': "2016", "academic_term": "2", "id": 1}
-    new = { 'academic_term': "2016", "academic_term": "2"}
+    old = {'academic_term': "2016", "academic_term": "2", "id": 1}
+    new = {'academic_term': "2016", "academic_term": "2"}
     dd = DictDiffer(old, new)
     assert len(dd.changed()) == 0
-
